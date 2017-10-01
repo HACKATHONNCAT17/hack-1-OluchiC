3fc669d838ec1f317ab40fc8c98fb26cfd47ba1a
OluchiC <Oluchi.j.c@Gmail.com>
2017-10-01 14:55:45 -0400

    New and Improved RaNdOo

--- page.js
+++ page.js
@@ -8,34 +8,38 @@ function tabFunction() {
   var objectArray = ["Dirt", "Lemons", "Rainbows", "DangerousNails", "SomeonesSoul",
   "LittleChildren", "MethaneGas", "HairFollicles", "ToeNails", "SustainableEnergy",
   "Chapstick", "Dorthy", "Windex", "Mayonnaise","ABeeHive"];
-  var randomIndex = Math.floor(Math.random() * nameArray.length);
+  var randomIndex = Math.floor(Math.random() * (nameArray.length - 1));
   var randomElement = nameArray[randomIndex];
-  var random2Index = Math.floor(Math.random() * nameArray.length);
+  var random2Index = Math.floor(Math.random() * (nameArray.length - 1));
   var random2Element = verbArray[random2Index];
-  var random3Index = Math.floor(Math.random() * nameArray.length);
-  var random3Element = objectArray[random2Index];
+  var random3Index = Math.floor(Math.random() * (nameArray.length - 1));
+  var random3Element = objectArray[random3Index];
   document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+  document.getElementById("flu").innerHTML = "";
+  document.getElementById("rap").innerHTML = "";
 
 }
 
 function oluFunction(){
 var  names = ["Ricky_ ","Chris_", "Omar_", "Devon_", "Evan_", " Filne_",
-"Drake_","Luchi_","Stevie_","Batmen_","Clark_Kent_", "Adele", "Trump", "Obama",
-"BillClinton"];
+"Drake_","Luchi_","Stevie_","Batmen_","Clark_Kent_", "Adele_", "Trump_", "Obama_",
+"BillClinton_","Brandon_", "Josh_"];
 
 var action = ["slapped", "punched", "kissed", "stabbed", "harassed", "challenged",
-"trapped","fought","smacked","killed","yelled_at","cherished", " cleaned ","peeded_on", " kneed ",""];
+"trapped","fought","smacked","killed","yelled_at","cherished", " cleaned ","headbutted", " kneed ","bit","picked_up",];
 
 var random = ["_a_cheecks", "_a_butt", "_an_elephant", "_a_chicken",
- "_the_bottom_of_his_shoe","_a_snicker"];
+ "_the_bottom_of_his_shoe","_a_snicker","_a_ water_bottle","_your_moms_bestfriend","_dinner",];
 
-var randomIndex = Math.floor(Math.random() * names.length);
+var randomIndex = Math.floor(Math.random() * (names.length - 1));
 var randomElement = names[randomIndex];
-var random2Index = Math.floor(Math.random() * action.length);
+var random2Index = Math.floor(Math.random() * (action.length - 1));
 var random2Element = action[random2Index];
-var random3Index = Math.floor(Math.random() * random.length);
-var random3Element = random[random2Index];
-document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+var random3Index = Math.floor(Math.random() * (random.length - 1));
+var random3Element = random[random3Index];
+document.getElementById("go").innerHTML = randomElement + random2Element  + random3Element;
+document.getElementById("flu").innerHTML = "";
+document.getElementById("rap").innerHTML = "";
 
 }
 
@@ -51,13 +55,15 @@ function derFunction(){
                  "Spoink", "Snorlax", "Slowbro", "Scatterbug",
                  "Sandslash", "Rayquaza", "Raticate", "Raichu","Plusle",
                  "Onix"];
- var randomIndex = Math.floor(Math.random() * pokemonName1.length);
+ var randomIndex = Math.floor(Math.random() * (pokemonName1.length - 1));
  var randomElement = pokemonName1[randomIndex];
- var random2Index = Math.floor(Math.random() * actionWord.length);
+ var random2Index = Math.floor(Math.random() * (actionWord.length - 1));
  var random2Element = actionWord[random2Index];
- var random3Index = Math.floor(Math.random() * pokemonName2.length);
- var random3Element = pokemonName2[random2Index];
+ var random3Index = Math.floor(Math.random() * (pokemonName2.length - 1));
+ var random3Element = pokemonName2[random3Index];
  document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+ document.getElementById("flu").innerHTML = "";
+ document.getElementById("rap").innerHTML = "";
 
 }
 
